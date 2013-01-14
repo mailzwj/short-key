37151e785c4abb7e0a82e8f9c38bd1871e6df821
nae <nae@gmail.com>
2013-01-14 11:50:02 +0800

    修改说明文案

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@
 exports.index = function(req, res){
   	res.render('index', {
   		title: '快速查看全球公用头像',
-  		content: '按住键盘【“Shift”】键的同时按下【“？”】键，弹出快速搜索浮层。按【“Esc”】键关闭浮层。'
+  		content: '按住键盘【“Shift” + “？”】键，弹出快速搜索浮层。按【“Esc”】键关闭浮层。'
   	});
 };
